c6417c558d348d1dd4ca53681d97697c2793a128
Sébastien BEAU <sebastien.beau@akretion.com>
2014-12-09 00:25:13 +0100

    [REF] extract odoo8 POS API into the plugin odoo8

--- pywebdriver/__init__.py
+++ pywebdriver/__init__.py
@@ -67,53 +67,6 @@ import views
 import plugins
 
 # ############################################################################
-# [Odoo 8.0] Route Section Emulating Odoo hw_proxy behaviour
-# ############################################################################
-@app.route('/hw_proxy/hello', methods=['GET'])
-@cross_origin()
-def hello_http():
-    return make_response('ping')
-
-
-@app.route('/hw_proxy/handshake', methods=['POST', 'GET', 'PUT', 'OPTIONS'])
-@cross_origin(headers=['Content-Type'])
-def handshake_json():
-    return jsonify(jsonrpc='2.0', result=True)
-
-
-@app.route('/hw_proxy/status_json', methods=['POST', 'GET', 'PUT', 'OPTIONS'])
-@cross_origin(headers=['Content-Type'])
-def status_json():
-    statuses = {}
-    for driver in drivers:
-        statuses[driver] = drivers[driver].get_status()
-    return jsonify(jsonrpc='2.0', result=statuses)
-
-
-@app.route(
-    '/hw_proxy/print_xml_receipt',
-    methods=['POST', 'GET', 'PUT', 'OPTIONS'])
-@cross_origin(headers=['Content-Type'])
-def print_xml_receipt_json():
-    """ For Odoo 8.0+"""
-    receipt = request.json['params']['receipt']
-    encoding = config.get('odoo', 'force_receipt_encoding')
-    if encoding != '':
-        drivers['escpos'].push_task('xml_receipt', receipt.encode(encoding))
-    else:
-        drivers['escpos'].push_task('xml_receipt', receipt)
-    return jsonify(jsonrpc='2.0', result=True)
-
-
-@app.route('/hw_proxy/log', methods=['POST', 'GET', 'PUT', 'OPTIONS'])
-@cross_origin(headers=['Content-Type'])
-def log_json():
-    arguments = request.json['params']['arguments']
-    print (' '.join(str(v) for v in arguments))
-    return jsonify(jsonrpc='2.0', result=True)
-
-
-# ############################################################################
 # Cups Route
 # ############################################################################
 @app.route('/cups/<method>', methods=['POST', 'GET', 'PUT', 'OPTIONS'])
