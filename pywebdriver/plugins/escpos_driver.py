eacf6f087777d7019647d3630a67d661b87ec8be
Sylvain LE GAL <sylvain.legal@grap.coop>
2016-04-21 17:44:45 +0200

    [FIX] restore odoo7 interfaces with TMT20 (escpos);

--- pywebdriver/plugins/escpos_driver.py
+++ pywebdriver/plugins/escpos_driver.py
@@ -27,6 +27,7 @@ from flask_cors import cross_origin
 from flask import request, jsonify, render_template
 from base_driver import ThreadDriver
 import usb.core
+import math
 
 
 meta = {
@@ -48,6 +49,7 @@ else:
         """ ESCPOS Printer Driver class for pywebdriver """
 
         def __init__(self, *args, **kwargs):
+            self.eprint = None
             self.vendor_product = None
             ThreadDriver.__init__(self, args, kwargs)
 
@@ -160,6 +162,183 @@ else:
                     config.getint('flask', 'port'),
                 )
                 self.receipt(msg)
+
+        # #####################################################################
+        # <Odoo Version 7>
+        def print_receipt_7(self, receipt):
+
+            def check(string):
+                return string is not True and bool(string) and string.strip()
+
+            def price(amount):
+                return (
+                    "{0:." +
+                    str(receipt['precision']['price']) + "f}").format(amount)
+
+            def money(amount):
+                return (
+                    "{0:." +
+                    str(receipt['precision']['money']) + "f}").format(amount)
+
+            def quantity(amount):
+                if math.floor(amount) != amount:
+                    return (
+                        "{0:." +
+                        str(receipt['precision']['quantity']) +
+                        "f}").format(amount)
+                else:
+                    return str(amount)
+
+            def printline(left, right='', width=40, ratio=0.5, indent=0):
+                lwidth = int(width * ratio)
+                rwidth = width - lwidth
+                lwidth = lwidth - indent
+
+                left = left[:lwidth]
+                if len(left) != lwidth:
+                    left = left + ' ' * (lwidth - len(left))
+
+                right = right[-rwidth:]
+                if len(right) != rwidth:
+                    right = ' ' * (rwidth - len(right)) + right
+
+                return ' ' * indent + left + right + '\n'
+
+            def print_taxes():
+                taxes = receipt.get('tax_details', [])
+                for tax in taxes:
+                    eprint.text(printline(
+                        tax['tax']['name'], price(tax['amount']), width=40,
+                        ratio=0.6))
+
+            if not self.eprint:
+                printers = self.connected_usb_devices()
+                if len(printers) > 0:
+                    self.eprint = Usb(
+                        printers[0]['vendor'], printers[0]['product'])
+                else:
+                    return
+
+            eprint = self.eprint
+
+            # Receipt Header
+            if receipt['company'].get('logo', False):
+                eprint.set(align='center')
+                eprint.print_base64_image(receipt['company']['logo'])
+                eprint.text('\n')
+            else:
+                eprint.set(align='center', type='b', height=2, width=2)
+                eprint.text(receipt['company']['name'] + '\n')
+
+            eprint.set(align='center', type='b')
+            if check(receipt['company'].get('contact_address', False)):
+                eprint.text(receipt['company']['contact_address'] + '\n')
+            if check(receipt['company'].get('phone', False)):
+                eprint.text(_(u'Tel: ') + receipt['company']['phone'] + '\n')
+            if check(receipt['company'].get('vat', False)):
+                eprint.text(_(u'VAT: ') + receipt['company']['vat'] + '\n')
+            if check(receipt['company'].get('email', False)):
+                eprint.text(receipt['company']['email'] + '\n')
+            if check(receipt['company'].get('website', False)):
+                eprint.text(receipt['company']['website'] + '\n')
+            if check(receipt.get('header')):
+                eprint.text(receipt['header'] + '\n')
+            if check(receipt.get('cashier')):
+                eprint.text('-' * 32 + '\n')
+                eprint.text(_(u'Served by ') + receipt['cashier'] + '\n')
+
+            # Orderlines
+            if config.getboolean('odoo', 'orderline_price_with_tax'):
+                orderline_price_field = 'price_with_tax'
+            else:
+                orderline_price_field = 'price_without_tax'
+            eprint.text('\n\n')
+            eprint.set(align='center')
+            for line in receipt['orderlines']:
+                pricestr = price(line[orderline_price_field])
+                if line['discount'] == 0\
+                        and line['unit_name'] == 'Unit(s)'\
+                        and line['quantity'] == 1:
+                    eprint.text(printline(
+                        line['product_name'], pricestr, ratio=0.6))
+                else:
+                    eprint.text(printline(line['product_name'], ratio=0.6))
+                    if line['discount'] != 0:
+                        eprint.text(printline(
+                            _(u'Discount: ') + str(line['discount'])+'%',
+                            ratio=0.6, indent=2))
+                    if line['unit_name'] == 'Unit(s)':
+                        eprint.text(printline(
+                            quantity(line['quantity']) + ' x ' +
+                            price(line['price']), pricestr, ratio=0.6,
+                            indent=2))
+                    else:
+                        eprint.text(printline(
+                            quantity(line['quantity']) + ' ' +
+                            line['unit_name'] + ' x ' + price(line['price']),
+                            pricestr, ratio=0.6, indent=2))
+
+            # Subtotal if the taxes are not included
+            taxincluded = True
+            if money(receipt['subtotal']) != money(receipt['total_with_tax']):
+                eprint.text(printline('', '-------'))
+                eprint.text(printline(
+                    _(u'Subtotal'), money(receipt['subtotal']), width=40,
+                    ratio=0.6))
+                print_taxes()
+                eprint.text(printline(
+                    _(u'Taxes'), money(receipt['total_tax']), width=40,
+                    ratio=0.6))
+                taxincluded = False
+
+            # Total
+            eprint.text(printline('', '-------'))
+            eprint.set(align='center', height=2)
+            eprint.text(printline(
+                _(u'         TOTAL'), money(receipt['total_with_tax']),
+                width=40, ratio=0.6))
+            eprint.text('\n\n')
+
+            # Paymentlines
+            eprint.set(align='center')
+            for line in receipt['paymentlines']:
+                eprint.text(printline(
+                    line['journal'], money(line['amount']), ratio=0.6))
+
+            eprint.text('\n')
+            eprint.set(align='center', height=2)
+            eprint.text(printline(
+                _(u'        CHANGE'), money(receipt['change']), width=40,
+                ratio=0.6))
+            eprint.set(align='center')
+            eprint.text('\n')
+
+            # Extra Payment info
+            if receipt['total_discount'] != 0:
+                eprint.text(printline(
+                    _(u'Discounts'), money(receipt['total_discount']),
+                    width=40, ratio=0.6))
+            if taxincluded:
+                print_taxes()
+                eprint.text(printline(
+                    _(u'Taxes'), money(receipt['total_tax']), width=40,
+                    ratio=0.6))
+
+            # Footer
+            if check(receipt.get('footer')):
+                eprint.text('\n'+receipt['footer']+'\n\n')
+            eprint.text(receipt['name']+'\n')
+            eprint.text(
+                str(receipt['date']['date']).zfill(2) +
+                '/' + str(receipt['date']['month']+1).zfill(2) +
+                '/' + str(receipt['date']['year']).zfill(4) +
+                ' ' + str(receipt['date']['hour']).zfill(2) +
+                ':' + str(receipt['date']['minute']).zfill(2))
+
+            eprint.cut()
+
+        # </Odoo Version 7>
+        # #####################################################################
 
     driver = ESCPOSDriver(app.config)
     drivers['escpos'] = driver
