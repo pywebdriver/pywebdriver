99fc6165decb8737e24e3fa294079742742bee75
andreparames <github@andreparames.com>
2018-05-08 10:50:35 +0200

    [FIX] Config file path in ConfigurePOS

--- desktop_shortcut.bat
+++ desktop_shortcut.bat
@@ -3,7 +3,7 @@ if "%~1"=="create" (
         copy %2\pkgs\pywebdriver\config\config.ini %2\config.ini
     )
     echo cd %2 > "%userprofile%\desktop\ConfigurePOS.bat"
-    echo notepad.exe "pkgs\pywebdriver\config\config.ini" >> "%userprofile%\desktop\ConfigurePOS.bat"
+    echo notepad.exe "config.ini" >> "%userprofile%\desktop\ConfigurePOS.bat"
     echo nssm.exe restart pywebdriver >> "%userprofile%\desktop\ConfigurePOS.bat"
 )
 if "%~1"=="delete" (
