bec6db0c56a9065cafac077dc3aafd6392bb3707
Pierrick Brun <pierrick.brun@akretion.com>
2021-09-20 16:50:25 +0200

    [UPD] install script with github releases mechanism

--- debian/install.sh
+++ debian/install.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
 # This script will install pywebdriver and a signed ssl certificate for localhost to avoid having to accept the self-signed certificate in the browser
 
-sudo apt-key adv --recv-keys --keyserver keyserver.ubuntu.com 305CB544DDFC7A97
-sudo apt-add-repository 'deb [arch=amd64] http://apt.akretion.com/ pywebdriver-nightly main'
+set -euxo pipefail
+
+export $(cat /etc/os-release | grep UBUNTU_CODENAME)
+wget https://github.com/akretion/pywebdriver/releases/latest/download/pywebdriver_${UBUNTU_CODENAME}.deb
 sudo apt update
-sudo apt install pywebdriver libnss3-tools
-cd /etc/pywebdriver
-sudo mkdir cert
-sudo chown pywebdriver:$USER cert
-cd cert
-sudo wget https://github.com/FiloSottile/mkcert/releases/download/v1.4.1/mkcert-v1.4.1-linux-amd64
-sudo chmod +x mkcert-v1.4.1-linux-amd64
-sudo ./mkcert-v1.4.1-linux-amd64 -install
-sudo ./mkcert-v1.4.1-linux-amd64 localhost 127.0.0.1
-cd /etc/pywebdriver
-sudo chown -R pywebdriver:$USER cert
-sudo sed -i 's/\/etc\/ssl\/certs\/ssl-cert-snakeoil.pem/\/etc\/pywebdriver\/cert\/localhost+1.pem/g' config.ini
-sudo sed -i 's/\/etc\/ssl\/private\/ssl-cert-snakeoil.key/\/etc\/pywebdriver\/cert\/localhost+1-key.pem/g' config.ini
+sudo apt install --reinstall --yes ./pywebdriver_${UBUNTU_CODENAME}.deb
+rm -f ./pywebdriver_${UBUNTU_CODENAME}.deb
+sudo mkdir -p /etc/pywebdriver/cert
+sudo chown pywebdriver:$(whoami) /etc/pywebdriver/cert
+sudo wget https://github.com/FiloSottile/mkcert/releases/download/v1.4.1/mkcert-v1.4.1-linux-amd64 -P /etc/pywebdriver/cert
+sudo chmod +x /etc/pywebdriver/cert/mkcert-v1.4.1-linux-amd64
+cd /etc/pywebdriver/cert
+sudo /etc/pywebdriver/cert/mkcert-v1.4.1-linux-amd64 -install
+sudo /etc/pywebdriver/cert/mkcert-v1.4.1-linux-amd64 localhost 127.0.0.1
+sudo chown -R pywebdriver:$(whoami) /etc/pywebdriver/cert
+sed -i -e '/cors_origins=/a\' -e 'sslcert=\/etc\/pywebdriver\/cert\/localhost+1.pem' /etc/pywebdriver/config.ini
+sed -i -e '/sslcert=/a\' -e 'sslkey=\/etc\/pywebdriver\/cert\/localhost+1-key.pem' /etc/pywebdriver/config.ini
 sudo service pywebdriver stop
 sudo service pywebdriver start
 
